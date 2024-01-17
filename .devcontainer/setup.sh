89ac77530b835dfddfab794fed0d4cffcef480ac
Brain Upgrade <77691372+brainupgrade-in@users.noreply.github.com>
2024-01-17 20:51:22 +0000

    refien

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -4,11 +4,11 @@ sudo usermod -aG docker $USER
 newgrp docker
 
 # Create a Kubernetes cluster using KIND
-kind create cluster --name dev-cluster
+sudo kind create cluster --name dev-cluster
 
-#mkdir -p $HOME/.kube
-#sudo cp -i /root/.kube/config $HOME/.kube/config
-#sudo chown $(id -u):$(id -g) $HOME/.kube/config
+mkdir -p $HOME/.kube
+sudo cp -i /root/.kube/config $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
 
 # Apply scenario setup
 kubectl apply -f /workspace/scenario.yaml
